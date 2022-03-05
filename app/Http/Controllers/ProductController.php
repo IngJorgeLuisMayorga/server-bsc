c2acdce5ac7f552a5c823dd6cc29cf2d2368052d
Jorge Luis Mayorga <jl.mayorga236@gmail.com>
2022-03-05 18:31:01 -0500

    Update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
         return json_encode($products);
     }
     public function getRecommended(Request $request) {
-        $products = Product::with(['category_skin_id', 'category_main_ingredient_id', 'category_solution_id', 'category_step_id', 'category_extra_id'])->random(3)->get();
+        $products = Product::with(['category_skin_id', 'category_main_ingredient_id', 'category_solution_id', 'category_step_id', 'category_extra_id'])::inRandomOrder()->limit(5)->get();
         return json_encode($products);
     }
     public function getById(Request $request, $id) {
